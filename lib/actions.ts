877e9a44b5fc9ee2af69c26b8f4a2ab4fe6d693f
Ahmed <gfg59204@gmail.com>
2023-07-06 01:02:35 +0400

    remove

--- lib/actions.ts
+++ lib/actions.ts
@@ -4,9 +4,9 @@ import { GraphQLClient } from "graphql-request";
 import Email from "next-auth/providers/email";
 
 const isProduction= process.env.NODE_ENV === "production"
-const apiUrl  = isProduction ?process.env.NEXT_PUBLIC_GRAFBASE_API_URL ||'':'http://127.0.0.1:4000/graphql'
+const apiUrl  = isProduction ?process.env.NEXT_PUBLIC_GRAFBASE_API_URL ||'':''
 const apiKey =isProduction?process.env.NEXT_PUBLIC_GRAFBASE_API_KEY || '' :'letmein'
-const  serverUrl = isProduction? process.env.NEXT_PUBLIC_GRAFBASE_SERVER_URL :'http://localhost:3000'
+const  serverUrl = isProduction? process.env.NEXT_PUBLIC_GRAFBASE_SERVER_URL :''
 
 const client = new GraphQLClient(apiUrl)
 
